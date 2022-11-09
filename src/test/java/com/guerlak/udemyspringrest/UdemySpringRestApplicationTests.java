43d5890e2176a4106081ac9061560eadc0832cc1
Rafa Guerlak <guerlak@hotmail.com>
2022-11-09 17:20:14 -0300

    start project

---
+++ src/test/java/com/guerlak/udemyspringrest/UdemySpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guerlak.udemyspringrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemySpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
